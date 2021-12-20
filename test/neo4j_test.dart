e4a74fd60be4ad76f25ba3f7ada0afec931f1b0d
Maadhav Sharma <maadhav2001@gmail.com>
2021-12-20 23:36:44 +0530

    tests added

--- test/neo4j_test.dart
+++ test/neo4j_test.dart
@@ -6,10 +6,11 @@ import 'package:neo4j/neo4j.dart';
 
 void main() {
   var neo4j = Neo4jClient.init(
-      app: Neo4jApp(
-    uri: Uri.parse("https://neo4j.igros.app"),
-    uid: "OkEZAJufEnRHIhYYZ3GuqPCyqUJ3",
-  ));
+    app: Neo4jApp(
+      uri: Uri.parse("https://neo4j.igros.app"),
+      uid: "OkEZAJufEnRHIhYYZ3GuqPCyqUJ3",
+    ),
+  );
   test('initialize instance', () {
     expect(neo4j.app.uri.toString(), "https://neo4j.igros.app");
   });
